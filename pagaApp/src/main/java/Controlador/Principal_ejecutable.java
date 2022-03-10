527ad7d7aa4c8a348e0e802e6510ca18d1e18720
Jesus Barros <jesus.reinaldo.barros@gmail.com>
2022-03-10 10:43:25 -0500

    Update

---
+++ pagaApp/src/main/java/Controlador/Principal_ejecutable.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import UI.Admi;
+
+/**
+ *
+ * @author jesusbarros
+ */
+public class Principal_ejecutable {
+    public static void main(String[] args) {
+        Admi ver = new Admi();
+        ver.setVisible(true);
+    }
+}
